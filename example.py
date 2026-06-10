ba9826dbb8402df44c16c8a01c1189b648e47d5a
chobits tai <chobits@itri.org.tw>
2026-06-10 15:19:44 +0800

    imu cali

--- example.py
+++ example.py
@@ -12,12 +12,16 @@ from sensor_msgs.msg import Image, Imu
 ACC_LSB_G   = 0.244 / 1000        # +/-8 g   -> 0.244 mg/LSB
 GYR_LSB_DPS = 70.0 / 1000         # 2000 dps -> 70 mdps/LSB
 
-ACC_TO_MS = ACC_LSB_G * 9.80665
+ACC_TO_MSS = ACC_LSB_G * 9.80665
 GYRO_TO_RPS = GYR_LSB_DPS * math.pi / 180
+
+with open('acc_cali.csv', 'r') as f:
+    acc_offset = tuple(float(x) for x in f.readline().split(','))
+    acc_scale = tuple(float(x) for x in f.readline().split(','))
 
 rclpy.init()
 node = rclpy.create_node('openmv')
-img_pub = node.create_publisher(Image, "mono", QoSProfile(depth=1, reliability=QoSReliabilityPolicy.BEST_EFFORT))
+img_pub = node.create_publisher(Image, "mono_left", QoSProfile(depth=1, reliability=QoSReliabilityPolicy.BEST_EFFORT))
 imu_pub = node.create_publisher(Imu, "imu", 200)
 
 # The on-cam script above, stored as a string (or read from a file).
@@ -45,8 +49,8 @@ with Camera("/dev/ttyACM0", ack=False, crc=False) as cam:
             imu_samples = list(struct.iter_unpack('hhhhhhHHH', data))
             for gx, gy, gz, ax, ay, az, ts_word0, ts_word1, _ in imu_samples:
                 imu_ts = ((ts_word0 << 8) | (ts_word1 >> 8)) & 0xFFFFFF
-                # print(gx*GYR_LSB_DPS, gy*GYR_LSB_DPS, gz*GYR_LSB_DPS, ax*ACC_LSB_G, ay*ACC_LSB_G, az*ACC_LSB_G, ts*0.025)
-                if imu_ts - prv_imu_ts > 200 or imu_ts - prv_imu_ts < 180:
+                # print(gx*GYR_LSB_DPS, gy*GYR_LSB_DPS, gz*GYR_LSB_DPS, ax*ACC_LSB_G, ay*ACC_LSB_G, az*ACC_LSB_G, imu_ts*0.025)
+                if prv_imu_ts > 0 and (imu_ts - prv_imu_ts > 200 or imu_ts - prv_imu_ts < 190):
                     print("imu ts gap", (imu_ts - prv_imu_ts)*25//1000, "ms", prv_imu_ts, imu_ts)
                 # ts_diff.append(ts-prv_imu_ts)
                 prv_imu_ts = imu_ts
@@ -54,9 +58,9 @@ with Camera("/dev/ttyACM0", ack=False, crc=False) as cam:
                 imu.header.frame_id = "body"
                 imu.header.stamp.sec = (imu_ts - imu_start_ts) *25 // 1_000_000
                 imu.header.stamp.nanosec = ((imu_ts - imu_start_ts) * 25 % 1_000_000) * 1000
-                imu.linear_acceleration.x = ax * ACC_TO_MS
-                imu.linear_acceleration.y = ay * ACC_TO_MS
-                imu.linear_acceleration.z = az * ACC_TO_MS
+                imu.linear_acceleration.x = (ax * ACC_TO_MSS - acc_offset[0]) * acc_scale[0]
+                imu.linear_acceleration.y = (ay * ACC_TO_MSS - acc_offset[1]) * acc_scale[1]
+                imu.linear_acceleration.z = (az * ACC_TO_MSS - acc_offset[2]) * acc_scale[2]
                 imu.angular_velocity.x = gx * GYRO_TO_RPS
                 imu.angular_velocity.y = gy * GYRO_TO_RPS
                 imu.angular_velocity.z = gz * GYRO_TO_RPS
